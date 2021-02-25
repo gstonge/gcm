6236612e2c8ce7fdbd29a6f4bc68e54defbed3ae
Guillaume St-Onge <guillaume.st-onge.4@ulaval.ca>
2021-02-25 08:38:05 -0500

    wip

--- gcm/ode.py
+++ gcm/ode.py
@@ -45,6 +45,29 @@ def get_state_meta(mmax, nmax, gm, pn):
     return (mmax,nmax,m,np.array(gm),np.array(pn),imat,nmat,pnmat)
 
 
+def get_state_meta_corr(mmax, nmax, Pm_n, Pn_m):
+    """Return a tuple that encapsulate all meta information about the structure
+
+    :param mmax: maximal membership
+    :param nmax: maximal group size >= 2
+    :param Pm_n: array for the membership following an edge starting from a
+                 group of size n, (nmax,mmax)
+    :param Pn_m: array for the group size following an edge starting from a
+                 node of membership m, (mmax,nmax).
+
+    :return state_meta: tuple of useful arrays describing the structure
+    """
+    mvec = np.arange(0,mmax+1)
+    imat = np.zeros((nmax+1,nmax+1))
+    nmat = np.zeros((nmax+1,nmax+1))
+    for n in range(2, nmax+1):
+        imat[n,0:n+1] = np.arange(n+1)
+        nmat[n,0:nmax+1] = np.ones(nmax+1)*n
+    Pn_m_mat = np.array([np.outer(Pn_m[m],np.ones(nmax+1)) for m in mvec])
+    return (mmax,nmax,mvec,np.array(Pm_n),np.array(Pn_m),imat,nmat,Pn_m_mat)
+
+
+
 @jit(nopython=True)
 def flatten(sm,fni,state_meta):
     nmax = state_meta[1]
@@ -71,8 +94,6 @@ def initialize(state_meta, initial_density=0.5):
     """
     mmax = state_meta[0]
     nmax = state_meta[1]
-    gm = state_meta[3]
-    pn = state_meta[4]
 
     sm = np.zeros(mmax+1)
     fni = np.zeros((nmax+1,nmax+1))
@@ -103,7 +124,7 @@ def infection_matrix(beta, nmax, args=()):
     return inf_mat
 
 
-def advance(sm, fni, tvar, inf_mat, state_meta):
+def advance(sm, fni, tvar, inf_mat, state_meta, corr=False):
     """advance integrates the ODE starting from a certain initial state and
     returns the new state.
 
@@ -112,12 +133,16 @@ def advance(sm, fni, tvar, inf_mat, state_meta):
     :param tvar: float for time variation.
     :param inf_mat: array of shape (nmax+1,nmax+1) representing the infection rate
     :param state_meta: tuple of arrays encoding information of the structure.
+    :param corr: bool to determine if there are correlations.
 
     return (sm,fni): tuple of state arrays later in time
     """
     v = flatten(sm,fni,state_meta)
     t = np.linspace(0,tvar)
-    vvec = odeint(vector_field,v,t,args=(inf_mat,state_meta))
+    if corr:
+        vvec = odeint(vector_field_corr,v,t,args=(inf_mat,state_meta))
+    else:
+        vvec = odeint(vector_field,v,t,args=(inf_mat,state_meta))
     return unflatten(vvec[-1],state_meta)
 
 
@@ -168,6 +193,60 @@ def vector_field(v, t, inf_mat, state_meta):
                                *(inf_mat[2:,:nmax] + rho))*fni[2:,:nmax]
     return np.concatenate((sm_field,fni_field.reshape((nmax+1)**2)))
 
+# @jit(nopython=True)
+def vector_field_corr(v, t, inf_mat, state_meta):
+    """vector_field returns the temporal derivative of a flatten state vector
+
+    :param v: array of shape (1,mmax+1+(nmax+1)**2) for the flatten state vector
+    :param t: float for time (unused)
+    :param inf_mat: array of shape (nmax+1,nmax+1) representing the infection rate
+    :param state_meta: tuple of arrays encoding information of the structure.
+
+    :returns vec_field: array of shape (1,(nmax+1)**2) for the flatten
+                        vector field.
+    """
+    mmax = state_meta[0]
+    nmax = state_meta[1]
+    m = state_meta[2]
+    Pm_n = state_meta[3]
+    Pn_m = state_meta[4]
+    imat = state_meta[5]
+    nmat = state_meta[6]
+    Pn_m_mat = state_meta[7]
+    sm = v[:mmax+1]
+    fni = v[mmax+1:].reshape(nmax+1,nmax+1)
+    fni_field = np.zeros(fni.shape) #matrix field
+    sm_field = np.zeros(sm.shape)
+
+    #calculate mean-field quantities
+    rm = np.zeros(mmax+1)
+    rm = np.sum(inf_mat[2:,:]*(nmat[2:,:]-imat[2:,:])*fni[2:,:]*\
+               Pn_m_mat[:,2:,:]/nmat[2:,:],axis=(1,2))
+    norm1 = np.sum((nmat[2:,:]-imat[2:,:])*fni[2:,:]*\
+               Pn_m_mat[:,2:,:]/nmat[2:,:],axis=(1,2))
+    rm = np.divide(rm,norm1, where=norm1!=0)
+    rhon = np.sum((m-1)*rm*sm*Pm_n,axis=1)
+    norm2 = np.sum(sm*Pm_n,axis=1)
+    rhon = np.divide(rhon,norm2, where=norm2!=0)
+    rhon_mat = np.outer(rhon,np.ones(nmax+1))
+
+    #contribution for nodes
+    #------------------------
+    sm_field = 1 - sm - sm*m*rm
+
+    #contribution for groups
+    #------------------------
+    #contribution from above
+    fni_field[2:,:nmax] += imat[2:,1:]*fni[2:,1:]
+    #contribution from equal
+    fni_field[2:,:] += (-imat[2:,:]
+                        -(nmat[2:,:] - imat[2:,:])
+                        *(inf_mat[2:,:] + rhon_mat[2:,:]))*fni[2:,:]
+    #contribution from below
+    fni_field[2:,1:nmax+1] += ((nmat[2:,:nmax] - imat[2:,:nmax])
+                               *(inf_mat[2:,:nmax] + rhon_mat[2:,:nmax]))*fni[2:,:nmax]
+
+    return np.concatenate((sm_field,fni_field.reshape((nmax+1)**2)))
 
 
 
